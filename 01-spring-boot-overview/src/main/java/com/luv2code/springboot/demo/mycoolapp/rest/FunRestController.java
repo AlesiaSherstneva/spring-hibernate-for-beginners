ad8e5fa7902df9b48a8c21a18cb8f82d5a3f0dcd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-04 22:01:45 +0300

    Inject custom properties

--- 01-spring-boot-overview/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/team-info")
+    public String getTeamInfo() {
+        return String.format("Coach: %s, Team name: %s", coachName, teamName);
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
