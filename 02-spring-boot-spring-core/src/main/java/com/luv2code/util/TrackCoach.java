2a42986f8b789b2eb7642f60a758b885b563d9a5
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-09 16:43:06 +0300

    Set up local lazy initialization on track coach

--- 02-spring-boot-spring-core/src/main/java/com/luv2code/util/TrackCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/util/TrackCoach.java
@@ -1,9 +1,11 @@
 package com.luv2code.util;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 /*@Primary*/
+@Lazy
 public class TrackCoach implements Coach {
     public TrackCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
