45f73fd24203718ee776c7cab8b2514f3eafa172
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-09 15:04:44 +0300

    Add some new implementations of Coach interface

---
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/util/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
