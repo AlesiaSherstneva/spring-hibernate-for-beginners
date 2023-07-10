eea7c7716c9bd2f4c52ef726b2b1b58118fa4859
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-10 23:17:08 +0300

    Make @PostConstruct and @PreDestroy methods for checking bean lifecycle

--- 02-spring-boot-spring-core/src/main/java/com/luv2code/util/CricketCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/util/CricketCoach.java
@@ -1,14 +1,26 @@
 package com.luv2code.util;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+/*@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)*/
 public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
     @Override
