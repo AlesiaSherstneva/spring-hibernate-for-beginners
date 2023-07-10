61e47c61f690e25d0c545382bf950237a0222edc
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-10 22:42:38 +0300

    Add new field and endpoint for checking bean scopes

--- 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // constructor dependency injection
     // use @Qualifier for choosing a coach implementation
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                          @Qualifier("cricketCoach") Coach theAnotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
-        this.myCoach = theCoach;
+        myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     // we can use @Primary annotation to choose coach implementation by default
@@ -44,5 +47,11 @@ public class DemoController {
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        // singleton returns true, prototype returns false
+        return "Comparing beans: myCoach == anotherCoach is " + (myCoach == anotherCoach);
     }
 }
--- 02-spring-boot-spring-core/src/main/java/com/luv2code/util/CricketCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/util/CricketCoach.java
@@ -1,8 +1,11 @@
 package com.luv2code.util;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
