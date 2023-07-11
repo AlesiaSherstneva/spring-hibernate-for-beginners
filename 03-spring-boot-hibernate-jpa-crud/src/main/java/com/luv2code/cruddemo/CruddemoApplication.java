16331596f8d88e97d6bcd26a24938f85b4bf793a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-12 02:53:34 +0300

    Add method for creating the student in main class

--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +14,19 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner() {
-        return runner -> System.out.println("Hello World");
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return runner -> {
+            createStudent(studentDAO);
+        };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("Creating new student object...");
+        Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+        System.out.println("Saving the student...");
+        studentDAO.save(tempStudent);
+
+        System.out.println("Saved student. Generated id: " + tempStudent.getId());
     }
 }
