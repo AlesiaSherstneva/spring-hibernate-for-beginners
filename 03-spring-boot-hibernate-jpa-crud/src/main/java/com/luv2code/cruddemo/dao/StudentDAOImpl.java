aea40d8ad9f4efa8c4c5d959c24c796247440610
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-15 15:46:16 +0300

    Update findAll() method to order objects alphabetically descending

--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -28,7 +28,9 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public List<Student> findAll() {
         // TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
-        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName", Student.class);
+        // TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName", Student.class);
+        TypedQuery<Student> theQuery = entityManager
+                .createQuery("FROM Student order by lastName desc", Student.class);
 
         return theQuery.getResultList();
     }
