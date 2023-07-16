84f188d3b073f48057074d2f8781135eab18c1eb
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-16 13:58:15 +0300

    Add method for updating the student in main class

--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -22,8 +22,21 @@ public class CruddemoApplication {
             // createMultipleStudents(studentDAO);
             // readStudent(studentDAO);
             // queryForStudents(studentDAO);
-            queryForStudentsByLastName(studentDAO);
+            // queryForStudentsByLastName(studentDAO);
+            updateStudent(studentDAO);
         };
+    }
+
+    private void updateStudent(StudentDAO studentDAO) {
+        int studentId = 1;
+        System.out.println("Getting student with id: " + studentId);
+        Student myStudent = studentDAO.findById(studentId);
+
+        System.out.println("Updating student ...");
+        myStudent.setFirstName("Scooby");
+        studentDAO.update(myStudent);
+
+        System.out.println("Updated student: " + myStudent);
     }
 
     private void queryForStudentsByLastName(StudentDAO studentDAO) {
