d3937a5c6b3e3328ccd3352f864a43b4b19cf22e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-16 13:42:40 +0300

    Add method for updating the student in DAO

--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -12,4 +12,6 @@ public interface StudentDAO {
     List<Student> findAll();
 
     List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
 }
--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -43,4 +43,10 @@ public class StudentDAOImpl implements StudentDAO {
 
         return theQuery.getResultList();
     }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
 }
