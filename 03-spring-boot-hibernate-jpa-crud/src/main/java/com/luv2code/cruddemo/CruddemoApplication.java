08d4fdb2863573ae0585548bc84582adb5683b85
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-16 14:25:09 +0300

    Add method for deleting the student in main class

--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -23,8 +23,15 @@ public class CruddemoApplication {
             // readStudent(studentDAO);
             // queryForStudents(studentDAO);
             // queryForStudentsByLastName(studentDAO);
-            updateStudent(studentDAO);
+            // updateStudent(studentDAO);
+            deleteStudent(studentDAO);
         };
+    }
+
+    private void deleteStudent(StudentDAO studentDAO) {
+        int studentId = 3;
+        System.out.println("Deleting student id: " + studentId);
+        studentDAO.delete(studentId);
     }
 
     private void updateStudent(StudentDAO studentDAO) {
