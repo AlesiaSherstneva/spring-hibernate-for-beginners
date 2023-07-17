fc4417ec03e779683750a21967352b6d92bad564
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-17 15:56:20 +0300

    Create new project for REST CRUD demo

---
+++ 04-spring-boot-rest-crud/src/main/java/com/luv2code/demo/SpringBootRestCrudApplication.java
@@ -0,0 +1,11 @@
+package com.luv2code.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestCrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestCrudApplication.class, args);
+    }
+}
