185ddd6d941eceb619f64ff5a5c311c658efe60e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-18 18:49:24 +0300

    Create student POJO class

---
+++ 04-spring-boot-rest-crud/src/main/java/com/luv2code/demo/entity/Student.java
@@ -0,0 +1,15 @@
+package com.luv2code.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String firstName;
+    private String lastName;
+}
