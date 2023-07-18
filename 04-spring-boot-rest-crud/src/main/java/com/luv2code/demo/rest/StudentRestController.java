4553a6d0bca2ca757838ea4bdf4a4baa7a4fe094
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-18 18:51:57 +0300

    Create student REST controller

---
+++ 04-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,9 @@
+package com.luv2code.demo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+}
