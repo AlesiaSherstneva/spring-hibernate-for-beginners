f8cb7d96f23abed115c782b0ebf4d90c5889bd39
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-19 15:44:57 +0300

    Create the handling for other exceptions

--- 04-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -43,4 +43,13 @@ public class StudentRestController {
         error.setTimestamp(System.currentTimeMillis());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exception.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
