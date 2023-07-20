7675e26d49ec1555f2adf505fdb8c1338949e46e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-20 23:46:25 +0300

    Create new project for REST CRUD employee app

---
+++ 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/CruddemoApplication.java
@@ -0,0 +1,11 @@
+package com.luv2code.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
+}
