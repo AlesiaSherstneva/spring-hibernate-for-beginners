305f60ac2c4ba4687ffd43c7986077c8bf330396
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-21 07:38:57 +0300

    Create employee entity

---
+++ 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
