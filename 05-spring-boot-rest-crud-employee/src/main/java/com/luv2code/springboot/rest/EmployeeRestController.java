b00f47d70ee13624a45b58546973fd6e1e641e32
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-21 08:31:34 +0300

    Inject service in the controller instead of DAO

--- 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/rest/EmployeeRestController.java
+++ 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/rest/EmployeeRestController.java
@@ -1,7 +1,7 @@
 package com.luv2code.springboot.rest;
 
-import com.luv2code.springboot.dao.EmployeeDAO;
 import com.luv2code.springboot.entity.Employee;
+import com.luv2code.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
