481b0b7b63b7e75dabcdfc1acf05edd2d5f683b4
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-21 08:05:44 +0300

    Create employee REST controller

---
+++ 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.luv2code.springboot.rest;
+
+import com.luv2code.springboot.dao.EmployeeDAO;
+import com.luv2code.springboot.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private final EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
