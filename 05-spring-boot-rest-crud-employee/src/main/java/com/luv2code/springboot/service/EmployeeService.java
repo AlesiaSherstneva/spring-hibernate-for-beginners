0757a7762834630cfcb6565e6427b3b76a7beebe
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-22 00:36:46 +0300

    Add CRUD operations to the service layer

--- 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeService.java
+++ 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeService.java
@@ -1,10 +1,15 @@
 package com.luv2code.springboot.service;
 
 import com.luv2code.springboot.entity.Employee;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface EmployeeService {
     List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
--- 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeServiceImpl.java
+++ 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.luv2code.springboot.dao.EmployeeDAO;
 import com.luv2code.springboot.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,5 +20,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
