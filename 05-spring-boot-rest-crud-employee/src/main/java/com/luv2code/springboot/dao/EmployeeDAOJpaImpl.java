c46eaad95021b4ee24a3919e9a8e6f76c1af5a82
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-22 00:20:56 +0300

    Add CRUD operations to DAO

--- 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/dao/EmployeeDAOJpaImpl.java
+++ 05-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/dao/EmployeeDAOJpaImpl.java
@@ -18,4 +18,20 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
         TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
         return theQuery.getResultList();
     }
+
+    @Override
+    public Employee findById(int theId) {
+        return entityManager.find(Employee.class, theId);
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return entityManager.merge(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        entityManager.remove(theEmployee);
+    }
 }
