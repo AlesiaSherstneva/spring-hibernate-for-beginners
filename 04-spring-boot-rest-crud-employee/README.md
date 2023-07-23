8ccf7d14287628e6206cf8c76cb9d374c63985ed
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-23 15:21:12 +0300

    Rename folders with CRUD projects

---
+++ 04-spring-boot-rest-crud-employee/README.md
@@ -0,0 +1,4 @@
+# Spring Boot - REST CRUD Employee
+
+В пятой части курса покрыты следующие темы:
+- Spring Data Jpa;
--- 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.service;
 
 import com.luv2code.springboot.dao.EmployeeDAO;
+import com.luv2code.springboot.dao.EmployeeRepository;
 import com.luv2code.springboot.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,18 @@ import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final EmployeeDAO employeeDAO;
+    // private final EmployeeDAO employeeDAO;
 
-    @Autowired
+    private final EmployeeRepository employeeRepository;
+
+/*    @Autowired
     public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
+    }*/
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
