03f4600eca3a0def105e6138b061c83507396020
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-23 15:40:04 +0300

    Inject the repository into the service instead of DAO

--- 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/service/EmployeeServiceImpl.java
@@ -1,24 +1,16 @@
 package com.luv2code.springboot.service;
 
-import com.luv2code.springboot.dao.EmployeeDAO;
 import com.luv2code.springboot.dao.EmployeeRepository;
 import com.luv2code.springboot.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    // private final EmployeeDAO employeeDAO;
-
     private final EmployeeRepository employeeRepository;
-
-/*    @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
-    }*/
 
     @Autowired
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
@@ -27,23 +19,30 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Employee theEmployee;
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            throw new RuntimeException("The employee with id " + theId + " not found");
+        }
+
+        return theEmployee;
     }
 
     @Override
-    @Transactional
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 }
