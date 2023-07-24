75ace2264f46cce480d427f16657ea85a22ed0c7
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-24 18:32:50 +0300

    Change default REST path

--- 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.luv2code.springboot.dao;
 
 import com.luv2code.springboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
