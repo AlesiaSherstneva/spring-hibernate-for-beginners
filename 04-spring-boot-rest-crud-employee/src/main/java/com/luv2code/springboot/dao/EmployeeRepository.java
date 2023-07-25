7df7fee3daabebaca224783b1184d4ebada9f7f0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-25 14:19:08 +0300

    Return basic path parameters

--- 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/dao/EmployeeRepository.java
@@ -4,6 +4,6 @@ import com.luv2code.springboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
