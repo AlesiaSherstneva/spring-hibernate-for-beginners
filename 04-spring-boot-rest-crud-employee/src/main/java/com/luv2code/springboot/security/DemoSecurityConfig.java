4ab35a9fa869bfae5971edd1ebe0cd1a17381116
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-26 06:51:41 +0300

    Restrict URLs based on roles

--- 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
+++ 04-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
@@ -2,11 +2,18 @@ package com.luv2code.springboot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class DemoSecurityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
@@ -29,5 +36,21 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 }
