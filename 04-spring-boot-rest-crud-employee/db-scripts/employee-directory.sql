b4c3dcf92bc4c925da5d5486452066ee3da2eefd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-07-28 22:14:34 +0300

    Update db-script for using custom authorities' tables

--- 04-spring-boot-rest-crud-employee/db-scripts/employee-directory.sql
+++ 04-spring-boot-rest-crud-employee/db-scripts/employee-directory.sql
@@ -33,15 +33,65 @@ DROP TABLE IF EXISTS `users`;
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+/*CREATE TABLE `users` (
     `username` varchar(50) NOT NULL,
     `password` char(68) NOT NULL,
     `enabled` tinyint NOT NULL,
     PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;*/
 
 --
 -- Inserting data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+
+-- Default passwords here are: fun123
+--
+
+/*INSERT INTO `users`
+VALUES ('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+       ('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+       ('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);*/
+
+--
+-- Table structure for table `authorities`
+--
+
+/*CREATE TABLE `authorities` (
+    `username` varchar(50) NOT NULL,
+    `authority` varchar(50) NOT NULL,
+    UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
+    CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;*/
+
+--
+-- Inserting data for table `authorities`
+--
+
+/*INSERT INTO `authorities`
+VALUES ('john','ROLE_EMPLOYEE'),
+       ('mary','ROLE_EMPLOYEE'),
+       ('mary','ROLE_MANAGER'),
+       ('susan','ROLE_EMPLOYEE'),
+       ('susan','ROLE_MANAGER'),
+       ('susan','ROLE_ADMIN');*/
+
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `members`;
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+    `user_id` varchar(50) NOT NULL,
+    `pw` char(68) NOT NULL,
+    `active` tinyint NOT NULL,
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `members`
 --
 -- NOTE: The passwords are encrypted using BCrypt
 --
@@ -50,28 +100,27 @@ CREATE TABLE `users` (
 -- Default passwords here are: fun123
 --
 
-INSERT INTO `users`
+INSERT INTO `members`
 VALUES ('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
        ('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
        ('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
-
 
 --
 -- Table structure for table `authorities`
 --
 
-CREATE TABLE `authorities` (
-    `username` varchar(50) NOT NULL,
-    `authority` varchar(50) NOT NULL,
-    UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
-    CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+CREATE TABLE `roles` (
+    `user_id` varchar(50) NOT NULL,
+    `role` varchar(50) NOT NULL,
+    UNIQUE KEY `authorities5_idx_1` (`user_id`,`role`),
+    CONSTRAINT `authorities5_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Inserting data for table `authorities`
+-- Inserting data for table `roles`
 --
 
-INSERT INTO `authorities`
+INSERT INTO `roles`
 VALUES ('john','ROLE_EMPLOYEE'),
        ('mary','ROLE_EMPLOYEE'),
        ('mary','ROLE_MANAGER'),
