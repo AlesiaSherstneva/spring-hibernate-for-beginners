304abfe7ab83db9fa41840bc4ab1e86ec62356fb
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-01 17:16:00 +0300

    Create new class for customer

---
+++ 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/Customer.java
@@ -0,0 +1,11 @@
+package com.luv2code.springmvc;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+    private String firstName;
+    private String lastName;
+}
