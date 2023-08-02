67360d257af64a2c69be9fa1c66f79ce7e940836
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-03 00:37:12 +0300

    Perform validation in the controller class

--- 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/CustomerController.java
+++ 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/CustomerController.java
@@ -1,8 +1,12 @@
 package com.luv2code.springmvc;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -10,5 +14,14 @@ public class CustomerController {
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processorForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                                BindingResult theBindingResult) {
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        return "customer-confirmation";
     }
 }
