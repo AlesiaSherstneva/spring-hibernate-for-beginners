4a2c85ff08862fb1ec34e6f1de50c9d608c73d3a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-04 15:10:48 +0300

    Add initbinder to trim leading and trailing whitespaces

--- 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/CustomerController.java
+++ 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/CustomerController.java
@@ -1,15 +1,24 @@
 package com.luv2code.springmvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/")
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
