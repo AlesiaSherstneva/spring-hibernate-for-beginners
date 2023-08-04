ce113f0d7cc3336fe5b81f571b882ebf9da547c3
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-04 15:45:28 +0300

    Add new field and number range validation

--- 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/Customer.java
+++ 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springmvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -13,4 +15,8 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be greater than or equals to zero")
+    @Max(value = 10, message = "must be less than or equals to ten")
+    private int freePasses;
 }
