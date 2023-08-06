1c93f9c2f6379033a75fd0346aa91d2e2fe3e8b8
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-06 14:37:05 +0300

    Make the integer field required

--- 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/Customer.java
+++ 06-spring-boot-spring-mvc-validation/src/main/java/com/luv2code/springmvc/Customer.java
@@ -13,9 +13,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equals to zero")
     @Max(value = 10, message = "must be less than or equals to ten")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only five chars/digits")
     private String postalCode;
