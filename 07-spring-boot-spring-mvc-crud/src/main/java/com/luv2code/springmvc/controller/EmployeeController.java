9a93df5ae6fcef44fd0ab7b749a69b56f90fd807
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-08 02:22:27 +0300

    Add code for receiving employees from db

--- 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/controller/EmployeeController.java
@@ -1,21 +1,25 @@
 package com.luv2code.springmvc.controller;
 
-import com.luv2code.springmvc.entity.Employee;
-import jakarta.annotation.PostConstruct;
+import com.luv2code.springmvc.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @GetMapping("/list")
     public String listEmployees(Model theModel) {
-        theModel.addAttribute("employees", theEmployees);
+        theModel.addAttribute("employees", employeeService.findAll());
         return "list-employees";
     }
 }
