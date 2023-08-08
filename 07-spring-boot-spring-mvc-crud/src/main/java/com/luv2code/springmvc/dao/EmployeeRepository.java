5d6a19317d3528dc0ca4ea5b6ce9507088141de0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-09 01:57:19 +0300

    Order employees by lastname

--- 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/dao/EmployeeRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springmvc.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByOrderByLastNameAsc();
 }
--- 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/service/EmployeeServiceImpl.java
+++ 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/service/EmployeeServiceImpl.java
@@ -20,7 +20,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> findAll() {
-        return employeeRepository.findAll();
+        return employeeRepository.findAllByOrderByLastNameAsc();
     }
 
     @Override
