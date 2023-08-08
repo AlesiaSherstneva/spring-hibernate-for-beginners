2107e6fa456f91a03a8f90951c6f84a09671dc75
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-09 01:15:48 +0300

    Add method and button for creating new employee

--- 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springmvc/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.luv2code.springmvc.controller;
 
+import com.luv2code.springmvc.entity.Employee;
 import com.luv2code.springmvc.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +24,17 @@ public class EmployeeController {
     public String listEmployees(Model theModel) {
         theModel.addAttribute("employees", employeeService.findAll());
         return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        theModel.addAttribute("employee", new Employee());
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
     }
 }
