f9fb3bf7341a87fef57a82c1f166e15b28c6b17e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-13 17:54:26 +0300

    Create new project for security demo

---
+++ 08-spring-boot-spring-mvc-security/README.md
@@ -0,0 +1,1 @@
+# Spring Boot - Spring MVC - Security
---
+++ 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/DemoSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.luv2code.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSecurityApplication.class, args);
+    }
+}
