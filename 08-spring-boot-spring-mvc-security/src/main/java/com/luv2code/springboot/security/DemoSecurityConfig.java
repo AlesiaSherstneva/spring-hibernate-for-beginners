b507578b907b8a9afbf3a3e1a701ac331cb85d02
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-14 21:56:24 +0300

    Modify security config to reference custom login form

--- 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -29,5 +31,16 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll());
+        return http.build();
     }
 }
