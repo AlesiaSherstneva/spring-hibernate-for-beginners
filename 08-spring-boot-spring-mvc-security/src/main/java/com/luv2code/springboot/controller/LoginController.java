bb11ef16757fcc3876b0b0109c34c80208a47e66
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-15 13:17:52 +0300

    Create login controller

---
+++ 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "plain-login";
+    }
+}
