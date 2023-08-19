ff63612dd4a76fb78214d45928a6cb3920483571
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-19 12:51:16 +0300

    Add security matchers for different roles

--- 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
@@ -37,6 +37,9 @@ public class DemoSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/showMyLoginPage")
