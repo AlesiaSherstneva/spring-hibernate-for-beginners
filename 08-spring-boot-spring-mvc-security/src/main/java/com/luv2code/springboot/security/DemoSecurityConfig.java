bb4263a9e840f4d937f6e1b944698a47652d6be6
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-21 23:40:36 +0300

    Update security configuration to use JDBC

--- 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/security/DemoSecurityConfig.java
@@ -4,34 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -50,4 +33,27 @@ public class DemoSecurityConfig {
                         .accessDeniedPage("/access-denied"));
         return http.build();
     }
+
+    /*    @Bean
+        public InMemoryUserDetailsManager userDetailsManager() {
+            UserDetails john = User.builder()
+                    .username("john")
+                    .password("{noop}test123")
+                    .roles("EMPLOYEE")
+                    .build();
+
+            UserDetails mary = User.builder()
+                    .username("mary")
+                    .password("{noop}test123")
+                    .roles("EMPLOYEE", "MANAGER")
+                    .build();
+
+            UserDetails susan = User.builder()
+                    .username("susan")
+                    .password("{noop}test123")
+                    .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                    .build();
+
+            return new InMemoryUserDetailsManager(john, mary, susan);
+        }*/
 }
