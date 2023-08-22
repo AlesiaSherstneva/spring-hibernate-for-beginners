71712a559264cd8fd513f27d62cba47ea75b01da
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-23 00:50:57 +0300

    Rename folders to organize them in order

--- 08-spring-boot-spring-security-with-jpa/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-security-with-jpa/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -5,36 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
-
     @GetMapping("/")
     public String showHome() {
-
         return "home";
     }
 
-    // add a request mapping for /leaders
-
     @GetMapping("/leaders")
     public String showLeaders() {
-
         return "leaders";
     }
 
-    // add request mapping for /systems
-
     @GetMapping("/systems")
     public String showSystems() {
-
         return "systems";
     }
-
 }
-
-
-
-
-
-
-
-
-
