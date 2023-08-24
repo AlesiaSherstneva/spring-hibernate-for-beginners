6738fc3b074b8640bf9efe4aa97bd07d8d3fe991
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-25 00:15:06 +0300

    Create the bean for command line runner

--- 09-spring-boot-jpa-advanced/src/main/java/com/luv2code/hibernatedemo/HibernateDemoApplication.java
+++ 09-spring-boot-jpa-advanced/src/main/java/com/luv2code/hibernatedemo/HibernateDemoApplication.java
@@ -1,11 +1,20 @@
 package com.luv2code.hibernatedemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(HibernateDemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello world");
+        };
     }
 }
