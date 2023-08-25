da9f16ac78dc8a2cfe4f5d93aefc04431817e69f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-08-26 00:11:35 +0300

    Add one-to-one uni relationship

--- 09-spring-boot-jpa-advanced/src/main/java/com/luv2code/hibernatedemo/entity/Instructor.java
+++ 09-spring-boot-jpa-advanced/src/main/java/com/luv2code/hibernatedemo/entity/Instructor.java
@@ -24,4 +24,8 @@ public class Instructor {
 
     @Column(name = "email")
     private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
 }
