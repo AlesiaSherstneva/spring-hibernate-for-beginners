a54e01ccf95a3ef830f5bdd185a23a236caed37a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-09-04 13:25:45 +0300

    Create class for course review

---
+++ 09-spring-boot-jpa-advanced/src/main/java/com/luv2code/hibernatedemo/entity/Review.java
@@ -0,0 +1,32 @@
+package com.luv2code.hibernatedemo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "review")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+}
