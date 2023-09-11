675df05a1f701311549e886891a38cf6d3f495d2
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-09-12 00:45:42 +0300

    Update command line runner for using account DAO

--- 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
+++ 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
@@ -1,5 +1,6 @@
 package com.luv2code.aopdemo;
 
+import com.luv2code.aopdemo.dao.AccountDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +11,13 @@ public class AopDemoApplication {
     public static void main(String[] args) {SpringApplication.run(AopDemoApplication.class, args);}
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
         return runner -> {
-            System.out.println("Hello World!");
+            demoTheBeforeAdvice(theAccountDAO);
         };
+    }
+
+    private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+        theAccountDAO.addAccount();
     }
 }
