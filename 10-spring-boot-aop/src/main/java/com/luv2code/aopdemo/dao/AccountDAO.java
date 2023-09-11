e7d4f519601160ca73478aa00b9d0ccfe44800d8
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-09-12 00:38:27 +0300

    Create account DAO

---
+++ 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,5 @@
+package com.luv2code.aopdemo.dao;
+
+public interface AccountDAO {
+    void addAccount();
+}
---
+++ 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,11 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO {
+    @Override
+    public void addAccount() {
+        System.out.println(getClass() + ": DOING MY WORK: ADDING AN ACCOUNT");
+    }
+}
