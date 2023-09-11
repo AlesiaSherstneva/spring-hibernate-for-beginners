2560ce420926801671f9e95f0b52f6cd816e6054
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-09-12 00:34:20 +0300

    Add command line runner to the main app class

--- 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
+++ 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
@@ -1,11 +1,18 @@
 package com.luv2code.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopDemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AopDemoApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(AopDemoApplication.class, args);}
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello World!");
+        };
     }
 }
