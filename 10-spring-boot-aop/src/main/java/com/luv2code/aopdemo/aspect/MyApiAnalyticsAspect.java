54acc1290c0960ecfc4bb0ba642c4b48415237b3
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-09-28 11:59:32 +0300

    Order the aspects

--- 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
     // @Before("forDaoPackage()")
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
--- 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,10 +2,12 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudLogAsyncAspect {
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void logToCloudAsync() {
