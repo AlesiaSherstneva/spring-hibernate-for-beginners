1bdb1209b25117b108a4a0bbfa30306aceba7e1f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-05 23:35:03 +0300

    Update main app for using new bean

--- 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
+++ 10-spring-boot-aop/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
 import com.luv2code.aopdemo.dao.MembershipDAO;
+import com.luv2code.aopdemo.service.TrafficFortuneService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +17,26 @@ public class AopDemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+    public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO,
+                                               MembershipDAO theMembershipDAO,
+                                               TrafficFortuneService theTrafficFortuneService) {
         return runner -> {
             // demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
             // demoTheAfterReturningAdvice(theAccountDAO);
             // demoTheAfterThrowingAdvice(theAccountDAO);
-            demoTheAfterAdvice(theAccountDAO);
+            // demoTheAfterAdvice(theAccountDAO);
+            demoTheAroundAdvice(theTrafficFortuneService);
         };
+    }
+
+    private void demoTheAroundAdvice(TrafficFortuneService theTrafficFortuneService) {
+        System.out.println("\nMain Program: demoTheAroundAdvice");
+        System.out.println("Calling getFortune()");
+
+        String data = theTrafficFortuneService.getFortune();
+        System.out.println("\nMy fortune is: " + data);
+
+        System.out.println("Finished");
     }
 
     private void demoTheAfterAdvice(AccountDAO theAccountDAO) {
