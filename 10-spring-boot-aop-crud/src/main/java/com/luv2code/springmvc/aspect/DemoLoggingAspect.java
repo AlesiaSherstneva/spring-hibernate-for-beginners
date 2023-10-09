80e46514a918beb3d7e510b5399712b62187204f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-09 18:59:32 +0300

    Add @Before advice for combined pointcut

--- 10-spring-boot-aop-crud/src/main/java/com/luv2code/springmvc/aspect/DemoLoggingAspect.java
+++ 10-spring-boot-aop-crud/src/main/java/com/luv2code/springmvc/aspect/DemoLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.luv2code.springmvc.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +12,7 @@ import java.util.logging.Logger;
 @Aspect
 @Component
 public class DemoLoggingAspect {
-    private Logger logger = Logger.getLogger(getClass().getName());
+    private final Logger logger = Logger.getLogger(getClass().getName());
 
     @Pointcut("execution(* com.luv2code.springmvc.controller.*.*(..))")
     private void forControllerPackage() {
@@ -26,4 +29,16 @@ public class DemoLoggingAspect {
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
     private void forAppFlow() {
     }
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint) {
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        logger.info("=====> in @Before: calling method: " + theMethod);
+
+        Object[] args = theJoinPoint.getArgs();
+        for (Object tempArg : args) {
+            logger.info("=====> argument: " + tempArg);
+        }
+    }
+
 }
