819cf7685610d0b3e388703ee9c0bcff4cf95ec8
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-09 18:48:33 +0300

    Add personal pointcuts for the controller, service and DAO packages

--- 10-spring-boot-aop-crud/src/main/java/com/luv2code/springmvc/aspect/DemoLoggingAspect.java
+++ 10-spring-boot-aop-crud/src/main/java/com/luv2code/springmvc/aspect/DemoLoggingAspect.java
@@ -1,10 +1,25 @@
 package com.luv2code.springmvc.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 @Aspect
 @Component
 public class DemoLoggingAspect {
+    private Logger logger = Logger.getLogger(getClass().getName());
 
+    @Pointcut("execution(* com.luv2code.springmvc.controller.*.*(..))")
+    private void forControllerPackage() {
+    }
+
+    @Pointcut("execution(* com.luv2code.springmvc.service.*.*(..))")
+    private void forServicePackage() {
+    }
+
+    @Pointcut("execution(* com.luv2code.springmvc.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
 }
