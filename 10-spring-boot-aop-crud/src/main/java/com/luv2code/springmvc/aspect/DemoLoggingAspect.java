920f9dc3843132858ba169f0b8569988b1aa2956
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-09 18:42:04 +0300

    Create aspect in the CRUD app

---
+++ 10-spring-boot-aop-crud/src/main/java/com/luv2code/springmvc/aspect/DemoLoggingAspect.java
@@ -0,0 +1,10 @@
+package com.luv2code.springmvc.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+
+}
