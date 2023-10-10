abe7764a927d7c7e424706d27814dfcf6be0be71
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-10 23:22:56 +0300

    Add @ToString method to the entity for logging view

--- 10-spring-boot-aop-crud/src/main/java/com/luv2code/springmvc/entity/Employee.java
+++ 10-spring-boot-aop-crud/src/main/java/com/luv2code/springmvc/entity/Employee.java
@@ -9,12 +9,14 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "employee")
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
