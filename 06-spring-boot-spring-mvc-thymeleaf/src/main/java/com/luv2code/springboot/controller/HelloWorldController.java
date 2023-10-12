a8ed5368bc101ec234028ac8a9d6359b51dfb6c6
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-12 22:42:38 +0300

    Add method with model to the controller

--- 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,17 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "hello-world";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+
+        String result = "Yo! " + theName;
+        model.addAttribute("message", result);
+
         return "hello-world";
     }
 }
