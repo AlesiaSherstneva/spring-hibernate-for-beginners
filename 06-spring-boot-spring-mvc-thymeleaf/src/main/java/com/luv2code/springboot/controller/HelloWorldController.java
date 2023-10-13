b2b6ee26e2c816f8b567141a75a92823c440536c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-13 22:49:56 +0300

    Add method with @RequestParam to the controller

--- 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -23,6 +24,17 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Yo! " + theName;
+        model.addAttribute("message", result);
+
+        return "hello-world";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName,
+                                          Model model) {
+        theName = theName.toUpperCase();
+
+        String result = "Hey my friend " + theName;
         model.addAttribute("message", result);
 
         return "hello-world";
