242789e6eb87215611d1b76958083c74ebad257a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-14 16:06:43 +0300

    Create student class to have a model object

---
+++ 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/model/Student.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+    private String firstName;
+    private String lastName;
+}
