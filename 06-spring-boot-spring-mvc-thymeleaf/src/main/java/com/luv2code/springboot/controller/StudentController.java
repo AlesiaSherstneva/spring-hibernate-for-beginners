40731e6cd154516acda94d460dd0681e1e6756ae
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-15 22:36:58 +0300

    Add method for process student form

--- 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/StudentController.java
+++ 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.luv2code.springboot.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -12,5 +14,11 @@ public class StudentController {
         Student theStudent = new Student();
         theModel.addAttribute("student", theStudent);
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        System.out.printf("theStudent: %s %s", theStudent.getFirstName(), theStudent.getLastName());
+        return "student-confirmation";
     }
 }
