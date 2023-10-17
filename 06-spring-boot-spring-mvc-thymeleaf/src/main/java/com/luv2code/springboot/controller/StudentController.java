0da047f783f3751a752e6031cfa95281761b24ef
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-17 20:57:52 +0300

    Inject list of countries from properties to the controller

--- 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/StudentController.java
+++ 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/StudentController.java
@@ -1,18 +1,25 @@
 package com.luv2code.springboot.controller;
 
 import com.luv2code.springboot.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         Student theStudent = new Student();
         theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries", countries);
         return "student-form";
     }
 
