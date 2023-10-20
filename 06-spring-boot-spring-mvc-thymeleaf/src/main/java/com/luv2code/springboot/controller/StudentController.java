b934ba8b520e744204f5d2c3a06cf0fb5d75a63c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-10-20 22:43:17 +0300

    Inject list of systems from properties to the controller

--- 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/StudentController.java
+++ 06-spring-boot-spring-mvc-thymeleaf/src/main/java/com/luv2code/springboot/controller/StudentController.java
@@ -18,12 +18,16 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         Student theStudent = new Student();
         theModel.addAttribute("student", theStudent);
         theModel.addAttribute("countries", countries);
         theModel.addAttribute("languages", languages);
+        theModel.addAttribute("systems", systems);
         return "student-form";
     }
 
